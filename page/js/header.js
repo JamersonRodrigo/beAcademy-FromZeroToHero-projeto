6b37a9d3d8483ee0d7241edc9ca787ca7dfed74a
Jamerson Rodrigo <jamersonrodrigo111@gmail.com>
2022-11-26 21:11:00 -0300

    initial commit

---
+++ page/js/header.js
@@ -0,0 +1,16 @@
+
+
+let menuMobile = document.getElementById("header")
+let aberto=false
+
+
+
+function ativaDesativa(){
+    if (aberto ===true){
+        menuMobile.classList.remove("ativado")
+        aberto=false
+    }else if(aberto===false){
+        menuMobile.classList.add("ativado")
+        aberto=true
+    }
+}
